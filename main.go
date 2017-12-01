818c9a2834a497c76285e3277e4309d9b6fc09a0
Jacob Shodd <jacobshodd@gmail.com>
2017-12-01 18:18:37 -0500

    cleaned up code, added let me google that for you functionality.

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"encoding/hex"
 	"encoding/json"
-	//	"fmt"
+	"fmt"
 	"github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
+	"net/url"
 	"strings"
 )
 
@@ -14,10 +14,20 @@ type Response struct {
 	Headers    map[string]string `json:"headers"`
 }
 
+func NewResponse(statusCode int, responseType, text string) Response {
+	headers := map[string]string{"Content-Type": "application/json"}
+	message := Message{ResponseType: responseType, Text: text}
+	return Response{StatusCode: statusCode, Body: message.String(), Headers: headers}
+}
+
 //Type Message
 type Message struct {
 	ResponseType string `json:"response_type"`
 	Text         string `json:"text"`
+}
+
+func (this *Message) String() string {
+	return fmt.Sprintf("{'response_type': '%s', 'text': \"%s\"}", this.ResponseType, this.Text)
 }
 
 //Type Request
@@ -34,7 +44,7 @@ type Request struct {
 	IsBase64Encoded       string            `json:"isBase64Encoded"`
 }
 
-type Info struct {
+type SlackInfo struct {
 	TeamId      string `json:"team_id"`
 	TeamDomain  string `json:"team_domain"`
 	ChannelId   string `json:"channel_id"`
@@ -47,36 +57,19 @@ type Info struct {
 	Text        string `json:"text"`
 }
 
-func toInfo(input string) Info {
-	list := strings.Split(input, "&")
-	data := make(map[string]string)
-	for _, x := range list {
-		temp := strings.Split(x, "=")
-		data[temp[0]] = temp[1]
-	}
-	for key, str := range data {
-		changes := make(map[string]string)
-		for index, value := range str {
-			if value == '%' {
-				decoded, _ := hex.DecodeString(str[index+1 : index+3])
-				changes[str[index:index+3]] = string(decoded)
-			}
-		}
-		for original, change := range changes {
-			data[key] = strings.Replace(data[key], original, change, -1)
-		}
-	}
-	result := Info{
-		TeamId:      data["team_id"],
-		TeamDomain:  data["team_domain"],
-		ChannelId:   data["channel_id"],
-		UserId:      data["user_id"],
-		UserName:    data["user_name"],
-		Command:     data["command"],
-		ResponseUrl: data["response_url"],
-		Token:       data["token"],
-		ChannelName: data["channel_name"],
-		Text:        data["text"]}
+func toInfo(input string) SlackInfo {
+	data, _ := url.ParseQuery(input)
+	result := SlackInfo{
+		TeamId:      data["team_id"][0],
+		TeamDomain:  data["team_domain"][0],
+		ChannelId:   data["channel_id"][0],
+		UserId:      data["user_id"][0],
+		UserName:    data["user_name"][0],
+		Command:     data["command"][0],
+		ResponseUrl: data["response_url"][0],
+		Token:       data["token"][0],
+		ChannelName: data["channel_name"][0],
+		Text:        data["text"][0]}
 	return result
 }
 
@@ -84,10 +77,22 @@ func Handle(evt json.RawMessage, ctx *runtime.Context) (interface{}, error) {
 	var request Request
 	json.Unmarshal(evt, &request)
 	info := toInfo(request.Body)
-	headers := map[string]string{"Content-Type": "application/json"}
-	message := Message{ResponseType: "ephemeral", Text: info.Text}
-	result := Response{StatusCode: 200, Body: "{'response_type': '" + message.ResponseType + "', 'text': '" + message.Text + "'}", Headers: headers}
-	//fmt.Println("sending response: \n", string(result))
-	return result, nil
+	if info.Command == "/infobot" {
+		switch info.Text {
+		case "oscprules":
+			text := "1. No spoilers\n2. No irrelevant discussions/offtopic chatter (okay in threads)\n3. Research before asking (http://www.catb.org/esr/faqs/smart-questions.html#before)\n4. NO DIRECT QUESTIONS ON MACHINES\n5. No shitposting/memes\n6. Try harder 7.\nPeople blatantly ignoring the rules will either get warned or kicked out from the channel.\n8. Don't add members to the channel, instead ask an admin."
+			return NewResponse(200, "ephemeral", text), nil
+		case "learn":
+			text := "NetSec Learning Resources: https://docs.google.com/spreadsheets/d/12bT8APhWsL-P8mBtWCYu4MLftwG1cPmIL25AEBtXDno/edit?usp=sharing"
+			return NewResponse(200, "ephemeral", text), nil
+		default:
+			text := "I don't know that one yet, this is just a proof of concept, but I do know:\n1) `/infobot learn` \n2) `/infobot oscprules`"
+			return NewResponse(200, "ephemeral", text), nil
+		}
+	} else if info.Command == "/lmgtfy" {
+		text := "http://lmgtfy.com/?q=" + strings.Replace(info.Text, " ", "+", -1)
+		return NewResponse(200, "in_channel", text), nil
+	}
+	return NewResponse(500, "ephemeral", "something went wrong..."), nil
 
 }
